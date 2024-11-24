12a16f618254479673a0380f6d4a34ad4533bfa7
karlohs <150075905+karlohs01@users.noreply.github.com>
2024-11-24 15:11:00 -0500

    updated module for separation of functions for upload/download speeds
    i think importing the functions into the client and server modules is needed in order to display the network stats. also, i know it only says upload and download, but there is also the file transfer and file size metrics that are displayed, which i think satisfies our 3 metric need.

--- analysisModule.py
+++ analysisModule.py
@@ -1,18 +1,51 @@
 # python module that collects statistics from our server module
 import psutil
 import time
-# creating a timer for collecting statistics
-# and then printing results after timer is over
-def collect_transfer_rates(duration=30):
-  start = psutil.net_io_counters()
-  time.sleep(duration)
-  end = psutil.net_io_counters()
-  sent_data = end.bytes_sent - start.bytes_sent
-  received_data = end.bytes_recv - start.bytes_recv
-  # metrics are in bytes per second
-  upload_rate = sent_data / duration
-  download_rate = received_data / duration
+import socket
 
-  return upload_rate, download_rate
+# tracking download speeds
+def download_speed(conn, filename, cwd):
+    # starting timer
+    start_timer = time.time() 
+    try:
+        size = 0
+        for i in range(0, len(file_data), BUFFER_SIZE):
+            chunk = file_data[i:i + BUFFER_SIZE]
+            conn.send(chunk)
+            size += len(chunk)
 
-print(collect_transfer_rates())
+        conn.send(b"EOF")
+        print(f'Sent file: {filename} to client.')
+
+        # calculating file transfer rate and download speed
+        end_timer = time.time()
+        duration = end_timer - start_timer
+        speed = (size / duration) * 8 / 1e6 # calculated in Mbps
+        # metrics that are printed are: file size, file transfer rate, and download speed
+        print(f'Download complete: {size} bytes in {duration: .2f} secs. Speed: {speed: .2f} Mbps')
+    
+    except Exception as e:
+        print(f'Error during download: {e}')
+# tracking upload speeds
+def upload_speed(conn, filename, cwd):
+    # starting timer
+    start_timer = time.time()
+    try:
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            if b"EOF" in data:
+                file_data += data.replace(b"EOF", b"")
+                break
+            file_data += data
+
+        size = len(file_data)
+        # ending timer
+        end_timer = time.time()
+        # calculations for file transfer rate and upload speed
+        duration = end_timer - start_timer
+        speed = (size / duration) * 8 / 1e6 # calculated in Mbps
+        # metrics that are printed are: file size, file transfer rate, and upload speed
+        print(f'Download complete: {size} bytes in {duration: .2f} secs. Speed: {speed: .2f} Mbps')
+
+    except Exception as e:
+        print(f'Error during download: {e}')
